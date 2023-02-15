7d9396c538add9f9b6c2204cf825e3b18988ffc7
Hugo Niescken <hugoniescken@MacBook-Pro.local>
2023-02-15 11:42:34 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-jau5d5r7zymxt44k.us.auth0.com',
+      clientId: 'H6qwa66sGSg72AeC5JCfoJeR6ADQ0Qhh',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth-button/auth-button.component.ts
@@ -0,0 +1,19 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { map, Observable } from 'rxjs';
+import { tap } from 'rxjs/internal/operators/tap';
+
+@Component({
+  selector: 'app-auth-button',
+  templateUrl: './auth-button.component.html',
+  styleUrls: ['./auth-button.component.scss']
+})
+export class AuthButtonComponent {
+
+  name$:Observable<string |  undefined>= this.auth.user$.pipe(map(User => User?.name))
+
+  constructor(@Inject(DOCUMENT) public document: Document,public auth: AuthService){
+
+  }
+}
